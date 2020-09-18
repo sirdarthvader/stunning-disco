a547ffa8171429bf13edc3aebacf0ecb73d0b66d
ashishcodes4 <ashishcodes4@gmail.com>
2020-09-18 17:27:41 +0530

    added files

--- api/routes/testApi.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-/* GET. */
-router.get("/test", function (req, res, next) {
-  res.send({ data: [1, 2, 3] });
-});
-
-module.exports = router;
